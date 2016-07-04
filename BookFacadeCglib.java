cbc9f0e36085aa128a9756256886a5a32dc5c5ba
xiongminglang <xiongminglang@gmail.com>
2016-07-05 00:16:49 +0800

    Add files via upload
    接口的实现

--- BookFacadeCglib.java
+++ BookFacadeCglib.java
@@ -1,11 +1,46 @@
-package com.cglib.dao;
+package com.cglib.Impl;
+
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
 
 /**
- * 使用JDK的cglib实现动态代理
  * Created by Administrator on 16-6-12.
  */
-public interface BookFacadeCglib {
+public class BookFacadeCglib implements MethodInterceptor {
 
-    public void addBook();
+    private Object target;
 
+
+    public Object getInstance(Object target) {
+        this.target = target;
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(this.target.getClass());
+
+        //使用回调方法
+        enhancer.setCallback(this);
+
+        //创建代理对象
+        return enhancer.create();
+    }
+
+
+    @Override
+    public Object intercept(Object o, Method method,
+                            Object[] objects,
+                            MethodProxy methodProxy) throws Throwable {
+
+        System.out.println("事物开始了"+">>>>>>>>>>>>>>");
+
+        /*
+         使用代理类执行方法
+         */
+        methodProxy.invokeSuper(o,objects);
+
+        System.out.println("事物结束了"+">>>>>>>>>>>>>>");
+
+        return null;
+    }
 }
